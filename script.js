79858bdd2550cdd6f40f0886110c0e69f46cac98
xislambkrx <140070869+xislambkrx@users.noreply.github.com>
2024-08-09 11:55:57 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 
+    // Script from index1.html
+    document.addEventListener('DOMContentLoaded', function () {
+      const filterButtons = document.querySelectorAll("#filter-buttons button");
+      const filterableCards = document.querySelectorAll("#filterable-cards .card");
+
+      const filterCards = (e) => {
+        document.querySelector("#filter-buttons .active").classList.remove("active");
+        e.target.classList.add("active");
+
+        filterableCards.forEach(card => {
+          if (card.dataset.name === e.target.dataset.filter || e.target.dataset.filter === "all") {
+            return card.classList.replace("hide", "show");
+          }
+          card.classList.add("hide");
+        });
+      }
+
+      filterButtons.forEach(button => button.addEventListener("click", filterCards));
+    });
